18fee67781cf63a2b460c8a5125657d98dca7ded
Ian Kinsey <ian@aikbix.com>
2024-08-27 23:49:51 -0400

    Baseline publisher

--- metagnosis/gateway/document.py
+++ metagnosis/gateway/document.py
@@ -1,6 +1,6 @@
-from contextlib import asynccontextmanager
+from datetime import datetime
 from json import dumps
-from typing import AsyncGenerator, List
+from typing import List
 from sqlite_vec import serialize_float32
 from .data_gateway import StorageGateway
 from ..log import log
@@ -50,12 +50,14 @@ class DocumentGateway(StorageGateway):
         if commit:
             await self.db.commit()
 
-    async def _get_docs(self, origin: str, limit: int = None) -> list[Document]:
+    async def _get_docs(self, origin: str, after: datetime, rank_threshold: int, limit: int = None) -> list[Document]:
         query = '''
         SELECT
             id, path, origin, title, score, categories, vector, processed, created, updated
         FROM document
         WHERE processed = FALSE
+        AND created > ?
+        AND rank_threshold >= ?
         AND origin = ?
         ORDER BY score DESC
         '''
@@ -63,7 +65,7 @@ class DocumentGateway(StorageGateway):
         if limit:
             query += f"\n LIMIT {limit}"
 
-        async with self.db.execute(query) as cursor:
+        async with self.db.execute(query, after, rank_threshold, origin) as cursor:
             return [
                 Document(
                     id=row[0],
@@ -91,13 +93,28 @@ class DocumentGateway(StorageGateway):
 
         await self.db.execute(query, ids)
 
-    @asynccontextmanager
-    async def get_documents_for_processing(self, origin, limit=None) -> AsyncGenerator[List[Document], None]:
-        async with self.transaction():
-            log.info("Retrieving documents from the queue for processing")
-            docs = await self._get_docs(origin)
+    async def get_documents_for_processing_multi(self, args_multi):
+        to_process = []
 
-            yield docs
+        class GetDocumentsForProcessingMulti:
+            async def __aenter__(s) -> List[List[Document]]:
+                await self.transaction().__aenter__()
+                log.info("Retrieving documents from the queue for processing")
+                s.to_process = []
 
-            log.info("Deleting docs from the queue")
-            await self._delete_docs([i.id for i in docs])
+                for args in args_multi:
+                    to_process.append(await self._get_docs(*args))
+
+                return to_process
+
+            async def __aexit__(s, exc_type, exc_val, exc_tb):
+                if exc_type is None:
+                    log.info("Document processing failed")
+                else:
+                    log.info("Document processing success")
+                    log.info("Deleting docs from the queue")
+                    await self._delete_docs([doc.id for docs in self.to_process for doc in docs])
+
+                await self.transaction().__aexit__(exc_type, exc_val, exc_tb)
+            
+        return GetDocumentsForProcessingMulti()
--- metagnosis/job/publisher.py
+++ metagnosis/job/publisher.py
@@ -1,15 +1,82 @@
+import numpy as np
+
+from sklearn.cluster import KMeans
+from scipy.spatial.distance import cdist
+from pydantic import BaseModel
 from .base import Job
+from .arxiv import TOPICS
 from ..gateway.document import DocumentGateway
+from ..models.document import Document
+
+
+class ArxivClusters(BaseModel):
+    ids: list[str]
+    vectors: np.array
+    kmeans: KMeans
+    clusters: int = len(TOPICS)
 
 
 class PublisherJob(Job):
     document: DocumentGateway
+    hn_limit: int = 25
+    arxiv_limit: int = 25
+    hn_rank_threshold: int = 100
 
     def __init__(self, document: DocumentGateway):
         self.document = document
 
     async def perform(self):
-        async with self.document.get_documents_for_processing() as docs:
-            # Rank HN posts and select the top ones
-            # Break arxiv into clusters, determine centers of each one, determine most novel
-            pass
+        args_multi = [
+            ("Hacker News", self.last_run_time, self.hn_rank_threshold, self.hn_limit)
+            ("arxiv", self.last_run_time, 0, None)
+        ]
+
+        async with self.document.get_documents_for_processing_multi(args_multi) as docs_multi:
+            docs = self.get_relevant_docs(docs_multi)
+    
+    def get_relevant_docs(self, docs_multi) -> list[Document]:
+        hn_docs, arxiv_docs = docs_multi
+        arxiv_docs = self.filter_arxiv_docs(arxiv_docs)
+
+        return hn_docs + arxiv_docs
+
+    def filter_arxiv_docs(self, docs: list[Document]):
+        arxiv = self.get_arxiv_cluster(docs)
+        ids = self.get_interesting_arxiv_papers(arxiv)
+
+        return [d for d in docs if d.id in ids]
+
+    def get_arxiv_cluster(self, docs: list[Document]) -> ArxivClusters:
+        data = {d.id: d.vector for d in docs}
+        ids = list(data.keys())
+        vectors = np.array(list(data.values()))
+        kmeans = KMeans(n_clusters=ArxivClusters.clusters, random_state=56)
+
+        kmeans.fit(vectors)
+
+        return ArxivClusters(
+            ids=ids,
+            vectors=vectors,
+            kmeans=kmeans
+        )
+    
+    def get_intersting_arxiv_papers(self, arxiv: ArxivClusters) -> set[str]:
+        centers = arxiv.kmeans.cluster_centers_
+        labels = arxiv.kmeans.labels_
+        distances = cdist(arxiv.vectors, centers, 'euclidean')
+        ids = set()
+
+        for i in range(arxiv.clusters):
+            # Novelty
+            indices = np.where(labels == i)[0]
+            vectors = vectors[indices]
+            inner_distances = cdist(vectors, vectors, 'euclidean')
+            avg_distances = np.mean(inner_distances, axis=1)
+            novel_index = np.argmax(avg_distances)
+            ids.add(arxiv.ids[indices[novel_index]])
+
+            # Centrality
+            index = np.argmin(distances[:, i]) 
+            ids.add(arxiv.ids[index])
+
+        return ids 
